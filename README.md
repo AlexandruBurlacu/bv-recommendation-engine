26fd1b1ff229b4d26ef84a7b1e21715d8afbfab7
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-07-23 10:45:10 +0300

    update README + add reshape_transform function

---
+++ README.md
@@ -0,0 +1,29 @@
+# Recommendation service
+
+Service is responsible for fetching data from Database Service and making recommendations of books based on selected criteria and emotional similarity of books.
+
+**More documentation to be added**
+
+## Installation
+
+Initially, you need `python` 3.5.x or higher and `virtualenv` to be installed on your machine.
+
+```bash
+    # to keep the namespace clean
+    virtualenv -p python3 .venv
+
+    # to install dependencies
+    pip install -r requirements.txt
+
+    # enter the virtual environment
+    source .venv/bin/activate
+```
+
+## Testing
+
+Currently, for testing purposes are used doctests, eventually unit tests may be added.
+
+
+## Style Guide
+
+Full PEP8 [here](https://www.python.org/dev/peps/pep-0008/) compliance. You may use PyLint. 1 tab must be 4 spaces wide. Don't use tab character. Configure your editor accordingly. Docstrings must follow NumPy/SciPy style [here](https://github.com/numpy/numpy/blob/master/doc/HOWTO_DOCUMENT.rst.txt).
--- logic.py
+++ logic.py
@@ -12,5 +12,59 @@ from utils import get_config, db_fetch
 
 CONFIG = get_config()
 
+def reshape_transform(objs):
+    """
+    Reshapes an iterable of 4-tuple to a dict of lists
+
+    Parameters
+    ----------
+    obj : iterable of (int, str, str, int)
+
+    Returns
+    -------
+    dict of str to list of (int, int)
+    {str: [(int, int)]}
+
+    Example
+    -------
+    >>> data = [(-1, 'fear', "tok", 0),
+    ...         (-1, 'sadness', "tok", 1),
+    ...         (1, 'surprise', "tok", 2),
+    ...         (-1, 'fear', "tok", 3),
+    ...         (1, 'surprise', "tok", 4)]
+    >>> reshape_transform(data) == {'anger': [],
+    ...                             'fear': [(-1, 0), (-1, 3)],
+    ...                             'joy': [],
+    ...                             'love': [],
+    ...                             'sadness': [(-1, 1)],
+    ...                             'surprise': [(1, 2), (1, 4)]}
+    True
+
+    """
+    get_sentiment_cat = lambda x: x[1]
+    get_sentiment_score = lambda x: x[0]
+    get_index = lambda x: x[3]
+
+    sentiment_timeline = {
+        "sadness": [],
+        "fear": [],
+        "joy": [],
+        "surprise": [],
+        "anger": [],
+        "love": []
+    }
+
+    for obj in objs:
+        data = (get_sentiment_score(obj), get_index(obj))
+        sentiment_timeline[get_sentiment_cat(obj)].append(data)
+
+    return sentiment_timeline
 
 
+# def pipeline():
+#     pass
+
+# def similarity_metric(sim_func):
+#     def _inner(base, variant):
+#         return {"sim": sim_func(base, variant)}
+#     return _inner
