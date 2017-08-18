18e69c3e6ecc4ed0df86f55543cbd17cf415b315
Alexandru Burlacu <alexburlacu96@gmail.com>
2017-08-18 15:48:40 +0300

    log enpoint entrance and exit + request headers

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ POST /api/v1/books/<book_id>/recommendations to get book recommendations for a b
 """
 
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask, request
 
 from logic import get_candidates, preprocess_resp
@@ -14,15 +16,25 @@ from utils import (get_book_by_id, search_by_auth_or_title,
                    get_config, db_fetch, make_query, get_sorted)
 
 app = Flask(__name__)
+
 addr = get_config()["mongo_rest_interface_addr"]
 
+handler = RotatingFileHandler('logs/info.log', maxBytes=10000000, backupCount=1)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter(
+    "[%(asctime)s] {%(funcName)s in %(pathname)s:%(lineno)d} %(levelname)s - %(message)s"))
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
 
 @app.route("/api/v1/books/<book_id>", methods=["GET"])
 def get_book(book_id):
     """Get specific book by it's ID"""
+    app.logger.info("Input: {}".format(book_id)) # [LOGGING]
+    app.logger.info("Headers: {}".format(list(request.headers))) # [LOGGING]
 
     response = preprocess_resp(get_book_by_id(addr, book_id))
 
+    app.logger.info("Output: 200 OK") # [LOGGING]
     return response
 
 @app.route("/api/v1/books", methods=["GET"])
@@ -30,7 +42,12 @@ def list_all_books():
     """Return all books with matching names for `author` or `title`"""
     search_query = request.args.get("q", "")
 
+    app.logger.info("Input: {}".format(search_query)) # [LOGGING]
+    app.logger.info("Headers: {}".format(list(request.headers))) # [LOGGING]
+
     response = preprocess_resp(search_by_auth_or_title(addr, search_query))
+
+    app.logger.info("Output: 200 OK") # [LOGGING]
 
     return response
 
@@ -38,6 +55,9 @@ def list_all_books():
 def recommend(book_id):
     """Returns 5 most similar books to the one which's `id` was passed as URL argument"""
     filters = request.get_json()["filters"]
+
+    app.logger.info("Input: {}".format(filters)) # [LOGGING]
+    app.logger.info("Headers: {}".format(list(request.headers))) # [LOGGING]
 
     base = json.loads(get_book_by_id(addr, book_id))["resp"][0]
     query = make_query(filters)
@@ -49,6 +69,8 @@ def recommend(book_id):
     base_title = base["metadata"]["title"]
 
     response = json.dumps(get_sorted(base_title, scores))
+
+    app.logger.info("Output: 200 OK") # [LOGGING]
 
     return response
 
